d74baf6d07b76685ce7d7bcfe7c9d8f23486e50f
Atherealy <hfggklm@gmail.com>
2024-07-18 03:41:21 +0300

    springJDBC-template исправления

--- springJDBС-template/src/main/java/service/CartService.java
+++ springJDBС-template/src/main/java/service/CartService.java
@@ -1,8 +1,5 @@
 package service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface CartService {
 
     boolean addToCartById(long userId, long productId);
--- springJDBС-template/src/main/java/service/ClientService.java
+++ springJDBС-template/src/main/java/service/ClientService.java
@@ -1,11 +1,9 @@
 package service;
 
 import model.Client;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
 public interface ClientService {
 
     long save(Client client);
